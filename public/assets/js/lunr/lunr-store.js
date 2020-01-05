37e4dfaf0d1fdc4ed43d997dd76530ada2b00f9d
goldsmithd <david_a_goldsmith@hotmail.com>
2020-01-05 11:13:42 +0000

    20200105a

--- public/assets/js/lunr/lunr-store.js
+++ public/assets/js/lunr/lunr-store.js
@@ -2,35 +2,35 @@ var store = [{
         "title": "Post: Gallery",
         "excerpt":"These are gallery tests for image wrapped in &lt;figure&gt; elements. To place a gallery add the necessary YAML Front Matter: gallery: - url: /assets/images/unsplash-gallery-image-1.jpg image_path: /assets/images/unsplash-gallery-image-1-th.jpg alt: \"placeholder image 1\" title: \"Image 1 title caption\" - url: /assets/images/unsplash-gallery-image-2.jpg image_path: /assets/images/unsplash-gallery-image-2-th.jpg alt: \"placeholder image 2\" title: \"Image 2 title caption\" -...","categories": ["Post Formats"],
         "tags": ["gallery","Post Formats","tiled"],
-        "url": "http://localhost:4000/post%20formats/post-gallery/",
+        "url": "http://localhost:4000/post%20formats/2010/09/09/post-gallery/",
         "teaser":null},{
         "title": "Post: Twitter Embed",
         "excerpt":"🎨 Finally got around to adding all my @procreateapp creations with time lapse videos https://t.co/1nNbkefC3L pic.twitter.com/gcNLJoJ0Gn &mdash; Michael Rose (@mmistakes) November 6, 2015   This post tests Twitter Embeds.  ","categories": ["Media"],
         "tags": ["content","embeds","media","twitter"],
-        "url": "http://localhost:4000/media/post-twitter-embeds/",
+        "url": "http://localhost:4000/media/2010/09/10/post-twitter-embeds/",
         "teaser":null},{
         "title": "Layout: Post with Sticky Table of Contents",
         "excerpt":"“Stick” table of contents to the top of a page by adding toc_sticky: true to its YAML Front Matter. --- toc: true toc_sticky: true --- HTML Elements Below is just about everything you’ll need to style in the theme. Check the source code to see the many embedded elements within...","categories": [],
         "tags": ["table of contents"],
-        "url": "http://localhost:4000/layout-table-of-contents-sticky/",
+        "url": "http://localhost:4000/2012/01/03/layout-table-of-contents-sticky/",
         "teaser":null},{
         "title": "Markup: Image Alignment",
         "excerpt":"Welcome to image alignment! The best way to demonstrate the ebb and flow of the various image positioning options is to nestle them snuggly among an ocean of words. Grab a paddle and let’s get started. The image above happens to be centered. The rest of this paragraph is filler for...","categories": ["Markup"],
         "tags": ["alignment","captions","content","css","image","markup"],
-        "url": "http://localhost:4000/markup/markup-image-alignment/",
+        "url": "http://localhost:4000/markup/2013/01/10/markup-image-alignment/",
         "teaser":null},{
         "title": "Markup: Another Post with Images",
         "excerpt":"Here are some examples of what a post with images might look like. If you want to display two or three images next to each other responsively use figure with the appropriate class. Each instance of figure is auto-numbered and displayed in the caption. Figures (for images or video) One...","categories": [],
         "tags": ["sample post","images","test"],
-        "url": "http://localhost:4000/markup-more-images/",
+        "url": "http://localhost:4000/2013/05/22/markup-more-images/",
         "teaser":"http://farm9.staticflickr.com/8426/7758832526_cc8f681e48_c.jpg"},{
         "title": "Layout: Header Video",
         "excerpt":"This post should display a header with a responsive video, if the theme supports it. Settings Parameter Required Description id Required ID of the video provider Required Hosting provider of the video, either youtube or vimeo YouTube To embed the following YouTube video at url https://www.youtube.com/watch?v=XsxDH4HcOWA (long version) or https://youtu.be/XsxDH4HcOWA...","categories": ["Layout","Uncategorized"],
         "tags": ["video","layout"],
-        "url": "http://localhost:4000/layout/uncategorized/layout-header-video/",
+        "url": "http://localhost:4000/layout/uncategorized/2017/01/23/layout-header-video/",
         "teaser":null},{
         "title": "Sample Diary Post",
-        "excerpt":"Welcome to image alignment! The best way to demonstrate the ebb and flow of the various image positioning options is to nestle them snuggly among an ocean of words. Grab a paddle and let’s get started. The image above happens to be centered. The rest of this paragraph is filler for...","categories": [],
+        "excerpt":"layout posts Welcome to image alignment! The best way to demonstrate the ebb and flow of the various image positioning options is to nestle them snuggly among an ocean of words. Grab a paddle and let’s get started. The image above happens to be centered. The rest of this paragraph is...","categories": ["Post Formats"],
         "tags": ["alignment","captions","content","css","image","markup"],
-        "url": "http://localhost:4000/sample-diary-post/",
+        "url": "http://localhost:4000/post%20formats/2020/01/01/sample-diary-post/",
         "teaser":null}]
